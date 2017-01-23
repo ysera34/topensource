6ae74c7097a2d85f8261696e0f84011d9b793623
ysera34 <ycyoon34@gmail.com>
2017-01-23 15:38:45 +0900

    twork1 skeleton

---
+++ TWork1/app/src/main/java/com/opensource/twork1/SalesFragment.java
@@ -0,0 +1,42 @@
+package com.opensource.twork1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yoon on 2017. 1. 23..
+ */
+
+public class SalesFragment extends Fragment {
+
+    public static SalesFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        SalesFragment fragment = new SalesFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sales, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
